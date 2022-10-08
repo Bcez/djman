e476ebd8d77cb9165c0f04944cbd33e16b20ef27
Bcez <80232341+Bcez@users.noreply.github.com>
2022-10-07 20:25:34 -0400

    Live Edit #4\

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ function setup(){
     video.hide();
 }
 
+// Sound System //
+
+song = "";
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+function play(){
+    song.play();
+}
+
+// Drawing System //
+
 function draw(){
     image(video, 0, 0, 600, 500);
 }
