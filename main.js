ccf1ac2b6a66f81d2cf86e3c687f67c5271fbff5
Bcez <80232341+Bcez@users.noreply.github.com>
2022-10-10 20:44:29 -0400

    Live edit #6

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.posenet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+// Ml5/P5.js Stuff //
+
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function modelLoaded(){
+    console.log('Posenet is loaded');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX + " rightWristY = "+rightWristY);
+    }
 }
 
 // Sound System //
@@ -16,6 +42,8 @@ function preload(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 // Drawing System //
