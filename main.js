e213fb9209a3eed0163a95051b30e1857406d835
Bcez <80232341+Bcez@users.noreply.github.com>
2022-10-14 20:28:27 -0400

    Live asdas

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
-
+scoreLeftWrist=0;
 function modelLoaded(){
     console.log('Posenet is loaded');
 }
@@ -22,6 +22,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = "+leftWristY);
@@ -50,4 +52,18 @@ function play(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill('black')
+    stroke('blue')
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+}
+
+
 }
