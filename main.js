c8f16ce057ff7d3ab7b8664352ce500baa68b9b7
Bcez <80232341+Bcez@users.noreply.github.com>
2022-10-17 20:22:42 -0400

    asd

--- main.js
+++ main.js
@@ -29,7 +29,8 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = "+leftWristY);
@@ -46,6 +47,41 @@ function draw(){
 
     fill('black')
     stroke('blue')
+if(scoreRightWrist > 0.2) {
+    circle(rightWristX,rightWristY,20)
+
+    if(rightWristY > 0 && rightWristY <= 100)
+    {
+    document.getElementById("speed").innerHTML = "Speed = 0.5x";
+    song.rate(0.5);
+}
+
+else if(rightWristY > 100 && rightWristY <= 200)
+{
+document.getElementById("speed").innerHTML = "Speed = 1x";
+song.rate(1);
+}
+
+else if(rightWristY > 200 && rightWristY <= 300)
+{
+document.getElementById("speed").innerHTML = "Speed = 1.5x";
+song.rate(1.5);
+}
+
+else if(rightWristY > 300 && rightWristY <= 400)
+{
+document.getElementById("speed").innerHTML = "Speed = 2x";
+song.rate(2);
+}
+else if(rightWristY > 400 && rightWristY <= 500)
+{
+document.getElementById("speed").innerHTML = "Speed = 2.5x";
+song.rate(2.5);
+}
+}
+
+
+
 
     if(scoreLeftWrist > 0.2){
         circle(leftWristX,leftWristY,20);
